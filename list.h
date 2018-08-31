7b2c4c1fae52b7ed14fda6863ae240cc8b37615a
Sergio Carbone <sercar88@gmail.com>
2018-08-31 18:41:17 -0500

    rama develop anadida

--- list.h
+++ list.h
@@ -15,15 +15,45 @@ class List {
     public:
         List();
 
-        T front();
-        T back();
-        void push_front(T value);
+        T front(){
+            if(empty()){
+            cout<<"lista vacia"<<endl;   
+            }else{
+                return start->prev->data;
+            }
+        };
+        T back(){
+          if(empty()){
+            cout<<"lista vacia"<<endl;   
+            }else{
+                
+                return start->data;
+            }  
+        };
+        void push_front(T value){
+            Node<T> *temp = new Node<T>;
+            temp->data=value;
+            if (empty())
+            {
+                temp->next=temp;
+                temp->prev=temp;
+                start  = temp;
+            }else{
+                temp->next=start;
+                temp->prev=start->prev;
+                start->prev=temp;
+                start=temp;
+            }
+
+        };
         void push_back(T value);
         void pop_front();
         void pop_back();
         T get(int position);
         void concat(List<T> &other);
-        bool empty();
+        bool empty(){
+            return (start->next==nullptr && start->prev==nullptr);
+        };
         int size();
         void clear();
         Iterator<T> begin();
